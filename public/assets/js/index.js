48b88c981600b3d9b407bad5d60fccba5fe46b33
Stephanie Giel <stephaniegiel@gmail.com>
2021-02-25 22:26:00 -0700

    fixed issue with choosing active note and not being able to write new note

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -54,8 +54,8 @@ const renderActiveNote = () => {
   hide(saveNoteBtn);
 
   if (activeNote.id) {
-    noteTitle.setAttribute('readonly', true);
-    noteText.setAttribute('readonly', true);
+    // noteTitle.setAttribute('readonly', true);
+    // noteText.setAttribute('readonly', true);
     noteTitle.value = activeNote.title;
     noteText.value = activeNote.title;
   } else {
@@ -157,8 +157,6 @@ const renderNoteList = async (notes) => {
   if (jsonNotes.length === 0) {
     noteListItems.push(createLi('No saved Notes', false));
   }
-
-  console.log("in here", jsonNotes)
 
   jsonNotes.forEach((note) => {
     const li = createLi(note.title);
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -6,13 +6,9 @@ router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
+// get * must come after other get routes
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
-
-// router.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../../public/index.html'));
-// });
-
 
 module.exports = router;
