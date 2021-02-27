f73c5a1ec68d5c99b85600ed5f8ee971db802bca
Stephanie Giel <stephaniegiel@gmail.com>
2021-02-27 16:04:37 -0700

    cleaned up server.js by removing unnecessary require statements

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-const fs = require('fs');
-const path = require('path');
-
 const express = require('express');
 
 const { notes }  = require('./db/db.json');
